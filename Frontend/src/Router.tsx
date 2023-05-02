986411b712d9a4069971871abcc3c8e5b3d48d43
hansolKim <kimhanol5334@gmail.com>
2023-05-02 16:54:51 +0900

    구조 수정

--- Frontend/src/Router.tsx
+++ Frontend/src/Router.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import Main from './pages/Main';
+import Test from './pages/Test';
 
 function Router() {
   return (
@@ -9,6 +10,7 @@ function Router() {
       <Nav />
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/test" element={<Test />} />
       </Routes>
     </BrowserRouter>
   );
