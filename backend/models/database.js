9867dfc29e96810619eb1c28bb30ccdc00ba162b
SEHEE PARK <sehii0327@gmail.com>
2023-05-06 05:46:30 +0900

    초기 환경세팅 서버 해결

---
+++ backend/models/database.js
@@ -0,0 +1,14 @@
+import { createConnection } from "typeorm";
+import dotenv from "dotenv";
+dotenv.config();
+
+const database = createConnection({
+  type: process.env.DB_CONNECTION,
+  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
+});
+
+export default database;
