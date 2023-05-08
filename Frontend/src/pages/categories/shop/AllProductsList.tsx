7ff74542985cad228a619dba682881f3742e8a92
Jacob <jhp4986@gmail.com>
2023-05-08 20:08:29 +0900

    [add][frontend][jacob] shop 페이지 link태그 path 설정

--- Frontend/src/pages/categories/shop/AllProductsList.tsx
+++ Frontend/src/pages/categories/shop/AllProductsList.tsx
@@ -1,20 +1,27 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 const AllProductsList: FC = () => {
   return (
     <div className="mx-20">
       <div className="mt-20 flex justify-center text-2xl font-medium">모든 제품</div>
       <div className=" flex justify-center">
-        <div className="mt-10 flex justify-evenly w-80 text-gray-400">
-          <span className="text-gray-400  transition-colors duration-800 hover:text-black cursor-pointer">
-            모든 제품
-          </span>
-          <span className="text-gray-400  transition-colors duration-800 hover:text-black cursor-pointer">
-            스킨케어
-          </span>
-          <span className="text-gray-400  transition-colors duration-800 hover:text-black cursor-pointer">
-            바디케어
-          </span>
+        <div className="mt-10 flex justify-evenly w-80 text-gray-400 ">
+          <Link to="/all-products">
+            <span className="text-gray-400  transition-colors duration-800 hover:text-black cursor-pointer ">
+              모든 제품
+            </span>
+          </Link>
+          <Link to="/skincare">
+            <span className="text-gray-400  transition-colors duration-800 hover:text-black cursor-pointer">
+              스킨케어
+            </span>
+          </Link>
+          <Link to="/bodycare">
+            <span className="text-gray-400  transition-colors duration-800 hover:text-black cursor-pointer">
+              바디케어
+            </span>
+          </Link>
         </div>
       </div>
       <div className="mt-20">
--- Frontend/src/pages/categories/shop/Shop.tsx
+++ Frontend/src/pages/categories/shop/Shop.tsx
@@ -1,4 +1,5 @@
 import React, { FC } from 'react';
+import { Link } from 'react-router-dom';
 
 const Shop: FC = () => {
   return (
@@ -6,15 +7,21 @@ const Shop: FC = () => {
       <div className="mt-20 flex justify-center text-2xl font-medium">Shop</div>
       <div className=" flex justify-center">
         <div className="mt-10 flex justify-evenly w-80 text-gray-400 ">
-          <span className="text-gray-400  transition-colors duration-800 hover:text-black cursor-pointer ">
-            모든 제품
-          </span>
-          <span className="text-gray-400  transition-colors duration-800 hover:text-black cursor-pointer">
-            스킨케어
-          </span>
-          <span className="text-gray-400  transition-colors duration-800 hover:text-black cursor-pointer">
-            바디케어
-          </span>
+          <Link to="/all-products">
+            <span className="text-gray-400  transition-colors duration-800 hover:text-black cursor-pointer ">
+              모든 제품
+            </span>
+          </Link>
+          <Link to="/skincare">
+            <span className="text-gray-400  transition-colors duration-800 hover:text-black cursor-pointer">
+              스킨케어
+            </span>
+          </Link>
+          <Link to="/bodycare">
+            <span className="text-gray-400  transition-colors duration-800 hover:text-black cursor-pointer">
+              바디케어
+            </span>
+          </Link>
         </div>
       </div>
       <div className="mt-20">
