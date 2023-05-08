019d9aad40e285567e956c8eb7cc11974bfb6ea7
Jacob <jhp4986@gmail.com>
2023-05-08 19:00:03 +0900

    [add][frontend][jacob] shop 페이지 레이아웃 구현중

--- Frontend/src/pages/categories/shop/AllProductsList.tsx
+++ Frontend/src/pages/categories/shop/AllProductsList.tsx
@@ -6,9 +6,15 @@ const AllProductsList: FC = () => {
       <div className="mt-20 flex justify-center text-2xl font-medium">모든 제품</div>
       <div className=" flex justify-center">
         <div className="mt-10 flex justify-evenly w-80 text-gray-400">
-          <span>모든 제품</span>
-          <span>스킨케어</span>
-          <span>바디케어</span>
+          <span className="text-gray-400  transition-colors duration-800 hover:text-black cursor-pointer">
+            모든 제품
+          </span>
+          <span className="text-gray-400  transition-colors duration-800 hover:text-black cursor-pointer">
+            스킨케어
+          </span>
+          <span className="text-gray-400  transition-colors duration-800 hover:text-black cursor-pointer">
+            바디케어
+          </span>
         </div>
       </div>
       <div className="mt-20">
--- Frontend/src/pages/categories/shop/Shop.tsx
+++ Frontend/src/pages/categories/shop/Shop.tsx
@@ -5,10 +5,16 @@ const Shop: FC = () => {
     <div className="mx-20">
       <div className="mt-20 flex justify-center text-2xl font-medium">Shop</div>
       <div className=" flex justify-center">
-        <div className="mt-10 flex justify-evenly w-80 text-gray-400">
-          <span>모든 제품</span>
-          <span>스킨케어</span>
-          <span>바디케어</span>
+        <div className="mt-10 flex justify-evenly w-80 text-gray-400 ">
+          <span className="text-gray-400  transition-colors duration-800 hover:text-black cursor-pointer ">
+            모든 제품
+          </span>
+          <span className="text-gray-400  transition-colors duration-800 hover:text-black cursor-pointer">
+            스킨케어
+          </span>
+          <span className="text-gray-400  transition-colors duration-800 hover:text-black cursor-pointer">
+            바디케어
+          </span>
         </div>
       </div>
       <div className="mt-20">
