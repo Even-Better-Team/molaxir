4cb8bafda02c629455be0bc29ba50d06c86e69a0
Jacob <jhp4986@gmail.com>
2023-05-08 17:13:59 +0900

    [add][frontend][박준현] shop 페이지 레이아웃 구현중

--- Frontend/src/pages/categories/shop/Shop.tsx
+++ Frontend/src/pages/categories/shop/Shop.tsx
@@ -1,7 +1,21 @@
 import React, { FC } from 'react';
 
 const Shop: FC = () => {
-  return <div>Shop</div>;
+  return (
+    <div className="mx-20">
+      <div className="mt-20 flex justify-center text-2xl font-medium">Shop</div>
+      <div className=" flex justify-center">
+        <div className="mt-10 flex justify-evenly w-80 text-gray-400">
+          <span>모든제품</span>
+          <span>스킨케어</span>
+          <span>바디케어</span>
+        </div>
+      </div>
+      <div className="mt-20">
+        <div className="text-xl">Best Items</div>
+      </div>
+    </div>
+  );
 };
 
 export default Shop;
