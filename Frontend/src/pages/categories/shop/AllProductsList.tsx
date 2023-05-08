b4fca43ba73e64c97815a172986eae43f84d452d
Jacob <jhp4986@gmail.com>
2023-05-08 17:36:30 +0900

    [add][frontend][jacob] 모든제품 페이지 레이아웃 구현중

--- Frontend/src/pages/categories/shop/AllProductsList.tsx
+++ Frontend/src/pages/categories/shop/AllProductsList.tsx
@@ -1,7 +1,21 @@
 import React, { FC } from 'react';
 
 const AllProductsList: FC = () => {
-  return <div>AllProductsList</div>;
+  return (
+    <div className="mx-20">
+      <div className="mt-20 flex justify-center text-2xl font-medium">모든 제품</div>
+      <div className=" flex justify-center">
+        <div className="mt-10 flex justify-evenly w-80 text-gray-400">
+          <span>모든 제품</span>
+          <span>스킨케어</span>
+          <span>바디케어</span>
+        </div>
+      </div>
+      <div className="mt-20">
+        <div className="text-xl">Best Items</div>
+      </div>
+    </div>
+  );
 };
 
 export default AllProductsList;
--- Frontend/src/pages/categories/shop/Shop.tsx
+++ Frontend/src/pages/categories/shop/Shop.tsx
@@ -6,7 +6,7 @@ const Shop: FC = () => {
       <div className="mt-20 flex justify-center text-2xl font-medium">Shop</div>
       <div className=" flex justify-center">
         <div className="mt-10 flex justify-evenly w-80 text-gray-400">
-          <span>모든제품</span>
+          <span>모든 제품</span>
           <span>스킨케어</span>
           <span>바디케어</span>
         </div>
